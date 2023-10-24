6a88815b2829e7ea5b2114b316682ea611d20804
deniz sağlam <83554334+Mne-Dev@users.noreply.github.com>
2023-10-24 13:02:37 +0300

    Add files via upload

--- rockpaperscissors.c
+++ rockpaperscissors.c
@@ -2,11 +2,14 @@
 #include <locale.h>
 #include <stdlib.h>
 #include <time.h>
+#include <ctype.h> 
+
 
 int main()
 {
 	setlocale(LC_ALL,"Turkish");
-
+	
+	printf("\e[1;1H\e[2J");
 	srand(time(NULL));
 	int pcplay = rand() % 3 + 1;
 	int play;
@@ -20,21 +23,41 @@ int main()
 	printf("Lütfen taş, kağıt ya da makas seçiniz \n1-Taş\n2-Kağıt\n3-Makas\n");
 	scanf("%d", &play);
 	
-	printf("%d",pcplay);
 
-	if(play == 1 && pcplay == 3 || play == 2 && pcplay == 1 || play == 3 && pcplay == 2)
-	{
-		printf("\nKazandın!\n");
-	}
-	else if(play == pcplay)
-	{
-		printf("\nBerabere!\n");
-	}
+	if (play <= 3 && play >= 1){
+
+                if(pcplay == 1)
+                {
+                printf("Bilgisayar taşı seçti\n");
+                }
+                else if(pcplay == 2)
+                {
+                        printf("Bilgisayar kağıtı seçti\n");
+                }
+                else
+                {
+                        printf("Bilgisayar makası seçti\n");
+                }
+
+                if(play == 1 && pcplay == 3 || play == 2 && pcplay == 1 || play == 3 && pcplay == 2)
+                {
+                        printf("\nKazandın!\n");
+                }
+                else if(play == pcplay)
+                {
+                        printf("\nBerabere!\n");
+                }
+                else
+                {
+                        printf("\nKaybettin!\n");
+                }
+        }
 	else
 	{
-		printf("\nKaybettin!\n");
+		printf("Hatalı giriş!\n");
+		return 0;
 	}
-
+	
 	return 0;
 }
 
